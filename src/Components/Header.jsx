6b8cf2498b4719245cd19988bdba5d421f7177fe
Aditi Sharma <aditisharmamay22@gmail.com>
2026-05-06 00:26:17 +0530

    added sign in and signup page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LOGO } from "../utils/constants";
 function Header() {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black ">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full">
       <img src={LOGO} alt="Netflix Logo" className="w-44" />
     </div>
   );
